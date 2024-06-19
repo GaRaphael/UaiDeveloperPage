a95db10c15c85e346bf492bcd53c738f2590207e
Gabriel Moreira Siqueira <109690729+GabrielMorSiq@users.noreply.github.com>
2024-06-19 18:48:08 -0300

    Update api.ts

--- api/api.ts
+++ api/api.ts
@@ -9,7 +9,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(async (config) => {
-  const token = localStorage.getItem('facitAccessToken')
+  const token = localStorage.getItem('facitAccessToken');
 
   if (token) {
     config.headers.Authorization = Bearer ${token};
